130dd0726f79cebd6e8c7377c372d02f2242bd84
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2024-03-09 20:31:49 +0000

    Fix typo 4

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2321,7 +2321,7 @@
   //#define BABYSTEP_DISPLAY_TOTAL          // Display total babysteps since last G28
 
   #define BABYSTEP_ZPROBE_OFFSET            // Combine M851 Z and Babystepping
-  #if All(BABYSTEP_ZPROBE_OFFSET, DE200_SCREEN_ANY)
+  #if ALL(BABYSTEP_ZPROBE_OFFSET, DE200_SCREEN_ANY)
     //#define BABYSTEP_HOTEND_Z_OFFSET      // For multiple hotends, babystep relative Z offsets
     #define BABYSTEP_GFX_OVERLAY            // Enable graphical overlay on Z-offset editor
   #endif
