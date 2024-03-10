82651c87c8bf52b34a38279e3dd89f36b90fdfb8
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2024-03-10 18:02:41 +0000

    Zero default input shaping values

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1216,14 +1216,14 @@
       #define SHAPING_FREQ_Y  31        // (Hz) The default dominant resonant frequency on the Y axis.
       #define SHAPING_ZETA_Y  0.10f     // Damping ratio of the Y axis (range: 0.0 = no damping to 1.0 = critical damping).
     #endif
-  #else // Only std head has measured damping factor, otherwise use Marlin defaults
+  #else // Only std head has measured damping factor, otherwise set off (user can set them to non-zero)
     #if ENABLED(INPUT_SHAPING_X)
-      #define SHAPING_FREQ_X  40        // (Hz) The default dominant resonant frequency on the X axis.
-      #define SHAPING_ZETA_X  0.15f     // Damping ratio of the X axis (range: 0.0 = no damping to 1.0 = critical damping).
+      #define SHAPING_FREQ_X  0         // (Hz) The default dominant resonant frequency on the X axis.
+      #define SHAPING_ZETA_X  0         // Damping ratio of the X axis (range: 0.0 = no damping to 1.0 = critical damping).
     #endif
     #if ENABLED(INPUT_SHAPING_Y)
-      #define SHAPING_FREQ_Y  40        // (Hz) The default dominant resonant frequency on the Y axis.
-      #define SHAPING_ZETA_Y  0.15f     // Damping ratio of the Y axis (range: 0.0 = no damping to 1.0 = critical damping).
+      #define SHAPING_FREQ_Y  0         // (Hz) The default dominant resonant frequency on the Y axis.
+      #define SHAPING_ZETA_Y  0         // Damping ratio of the Y axis (range: 0.0 = no damping to 1.0 = critical damping).
     #endif
   #endif
   //#define SHAPING_MIN_FREQ  20        // By default the minimum of the shaping frequencies. Override to affect SRAM usage.
